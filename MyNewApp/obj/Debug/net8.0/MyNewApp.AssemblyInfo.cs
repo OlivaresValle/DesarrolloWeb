202fe3517b5f9d01a1c7917003f3e7070d13b270
Tomás Olivares Valle <tomasolivaresvalle@gmail.com>
2024-07-11 19:00:49 -0400

    ejercicios de nest

--- MyNewApp/obj/Debug/net8.0/MyNewApp.AssemblyInfo.cs
+++ MyNewApp/obj/Debug/net8.0/MyNewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bad954c50d03bd250b05c24c42dcb4b592a018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac22088a57f4172592794d1cedb091dccf16619a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
